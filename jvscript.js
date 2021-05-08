6f242161ecb3326a6e34c9ed4dfe3d378cc90a86
LiandaDuanDal <ln387408@dal.ca>
2021-05-08 18:05:35 -0300

    version 2.0 adds progress and full screen
    version 2.0 adds progress and full screen

--- jvscript.js
+++ jvscript.js
@@ -1,22 +1,45 @@
+(function(window, document){
+
 // 获取视频窗口对象
 var video = document.querySelector('.video');
 // 获取进度条对象
-var juice = document.querySelector('.orange-juice');
+var juice = document.querySelector(".orange-juice");
+// orange-bar
+var orange_bar = document.querySelector('.orange-bar');
 // 获取播放停止按钮对象
 var btn = document.getElementById('play-pause');
 // 获取nextstep 向右跳跃
 var btn_nextstep = document.getElementById('nextstep');
 // 获取previousstep 向左跳跃
 var btn_previousstep = document.getElementById('previousstep')
+// get vide0 container
+var videoContainer = document.getElementById("videoContainer");
+// 
+var fullScreenBtn = document.getElementById("fullScreenBtn");
+// jump interval-->control gap for forward and backword
+var jump_gap = 10
+var fullScreenFlag = false; 
+// new
+// function getProgress(){
+//     var percent = video.currentTime / video.duration;
+//     playProgress.style.width = percent * (orange_bar.offsetWidth) - 2 + "px";
+//     showProgress.innerHTML = (percent * 100).toFixed(1) + "%";
+// }
 
-var jump_gap = 10 
+// new
+
+
+// console.log(orange_bar == null)
+
 // 按下播放停止按钮时的按钮变化和视频pause play切换
 function togglePlayPause() {
     if(video.paused) {
         btn.className = 'pause';
+        progressFlag = setInterval(getProgress, 60);
         video.play();
     } else {
         btn.className = 'play';
+        clearInterval(progressFlag);
         video.pause();
     }
 }
@@ -36,12 +59,58 @@ function previousStep(){
     }
 
 }
-//拖动进度条
-function move(){
+//full screen
+function fullScreen(){
+    if(fullScreenFlag){
+        videoContainer.webkitCancelFullScreen();
+    }
+    else{
+        videoContainer.webkitRequestFullscreen();
+    }
+}
+//拖动进度条****
+// video的播放条
+function getProgress(){
+    var juicePos = video.currentTime / video.duration;
+    juice.style.width = juicePos * 100 + "%";
+    juice.innerHTML = (juicePos * 100).toFixed(1) + "%";
+}
+// 鼠标在播放条上点击时进行捕获并进行处理
+function videoSeek(e){
+    if(video.paused || video.ended){
+        togglePlayPause();
+        enhanceVideoSeek(e);
+    }
+    else{
+        enhanceVideoSeek(e);
+    }
 
 }
+function enhanceVideoSeek(e){
+    clearInterval(progressFlag);
+    var length = event.pageX - orange_bar.offsetLeft;
+    var percent = length / orange_bar.offsetWidth;
+    console.log(orange_bar.offsetLeft)
+    console.log(orange_bar.offsetWidth)
+    console.log(event.pageX)
+    // var length = video.duration;
+    // var percent = video.currentTime / video.duration;
+    juice.style.width = percent * (orange_bar.offsetWidth) - 2 + "px";
+    video.currentTime = percent * video.duration;
+    progressFlag = setInterval(getProgress, 60);
+}
+// ****
 
 
+
+fullScreenBtn.onclick = function(){
+    if(fullScreenFlag){
+        videoContainer.webkitCancelFullScreen();
+    }
+    else{
+        videoContainer.webkitRequestFullscreen();
+    }
+}
 
 
 
@@ -72,3 +141,29 @@ video.addEventListener('timeupdate', function() {
         togglePlayPause();
     }
 });
+
+
+//try orange-juice
+// juice.addEventListener('change')
+//单击视频触发停止和播放
+video.addEventListener('click', function() {
+    console.log('stop star clicked!!!')
+togglePlayPause()
+});
+
+// add listener to change progress
+// if(juice != null){
+// juice.addEventListener('mousedown',function(){
+//     console.log('progress bar clicked!!');
+//     videoSeek();
+// });}else{
+//     console.log("juice is null!!");
+// }
+orange_bar.addEventListener('mousedown',function(){
+    console.log('progress bar clicked!!');
+    videoSeek();
+});
+
+
+}(this, document))
+
